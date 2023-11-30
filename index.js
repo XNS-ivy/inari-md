538d83f6981f353a793190ff0bced613d8803d4b
XNS-ivy <ayramusic.nightcore@gmail.com>
2023-11-30 10:03:57 +0700

    creating sockets using express framework

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const sessionPath = './session';
+const readLine = require('readline');
 
 async function inariSock() {
   const { state, saveCreds } = await useMultiFileAuthState(sessionPath);
   const inari = await makeWASocket({
     printQRInTerminal: true,
+    qrTimeout: 30000,
     auth: state,
     logger: pino({ level: 'silent' }),
     browser: ['Inari-MD','FireFox','1.0.0']
@@ -18,9 +20,26 @@ async function inariSock() {
     if(connection === 'open'){
       console.log('is open');
     } else if (connection === 'close'){
-      await inariSock();
+      try {
+        await inariSock();
+      } catch (error) {
+        Boom.badimplementation();
+        console.error('',error);
+      }
     }
   });
   inari.ev.on('creds.update', saveCreds);
 }
-inariSock();
+app.use(async,(req, res, next) => {
+  res.redirect('/');
+});
+app.get('/',async (req, res) => {
+  res.send('Hello World');
+  await inariSock();
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, async () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
