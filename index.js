c50fd3ba8a77aef78fd481a63b5a01467d32184f
XNS-ivy <ayramusic.nightcore@gmail.com>
2023-12-02 15:45:47 +0700

    base connectiom

--- index.js
+++ index.js
@@ -5,37 +5,62 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const sessionPath = './session';
-const readLine = require('readline');
+
+const pairingCode = process.argv.includes('--cd');
 
 async function inariSock() {
   const { state, saveCreds } = await useMultiFileAuthState(sessionPath);
   const inari = await makeWASocket({
-    printQRInTerminal: true,
+    printQRInTerminal: !pairingCode,
     qrTimeout: 30000,
     auth: state,
     logger: pino({ level: 'silent' }),
     browser: ['Inari-MD','FireFox','1.0.0']
   });
+  if(pairingCode && !inari.authState.creds.regustered){
+    const question = () => new Promise ((resolve) => {
+      const readLine = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      
+      readline.question('Input Your Phone Number: +',(resolve) => {
+        resolve(answer);
+        readline.close();
+      });
+    });
+    const pNumber = await question();
+    setTimeout(async() => {
+      const code = await inari.requestPairingCode(Phone);
+      console.log('Your Pairing Code : '+ code);
+    }, 5000);
+  }
   inari.ev.on('connection.update',async  ({connection}) => {
     if(connection === 'open'){
       console.log('is open');
     } else if (connection === 'close'){
+      setTimeout(() =>{
+        console.log('Connection Cloded, Try To Recconect\n\n');
+      }, 3000);
       try {
         await inariSock();
       } catch (error) {
-        Boom.badimplementation();
-        console.error('',error);
+        console.error('Error Reconnecting\n\n',error);
+        throw error;
       }
     }
   });
   inari.ev.on('creds.update', saveCreds);
 }
-app.use(async,(req, res, next) => {
-  res.redirect('/');
-});
-app.get('/',async (req, res) => {
-  res.send('Hello World');
+
+app.get('/',async(req, res) => {
+  try {
   await inariSock();
+  res.send('Check your server logs for connection status.');
+  } catch (error) {
+    console.error('Error Start Server:\n\n',error);
+    throw error;
+  }
 });
 
 // Start the server
